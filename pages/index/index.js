cf1418009a3784c2ec1573a05801b23140258077
admeng.liu <admeng.liu@qunar.com>
2018-10-20 12:39:53 +0800

    先上版本微信小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,56 +4,68 @@ var app = getApp()
 Page({
   data: {
     infoMess: '',
-    userName: '',
-    userN:'',
-    passWd: '',
-    passW:''
+    calName: '',
+    calN: ''
   },
-  //用户名和密码输入框事件
-  userNameInput:function(e){
+  calInput: function(e) {
     this.setData({
-      userN:e.detail.value
+      calN: e.detail.value
     })
   },
-  passWdInput:function(e){
-    this.setData({
-      passW:e.detail.value
-    })
-  },
-  //登录按钮点击事件，调用参数要用：this.data.参数；
-  //设置参数值，要使用this.setData({}）方法
-  loginBtnClick:function(){
-    if(this.data.userN.length == 0 || this.data.passW.length == 0){
+  calBtnClick: function() {
+    if (this.data.calN.length == 0) {
       this.setData({
-        infoMess:'温馨提示：用户名和密码不能为空！',
+        infoMess: '请输入税前工资',
       })
-    }else{
+    } else {
+      var total = this.data.calN;
+      if (total >= 25401) {
+        total = (total - 25401 * 0.222);
+      } else {
+        total = (total - total * 0.222);
+      }
+
+      var afterTotal = 0;
+      if (total > 5000) {
+        var tax = total - 5000;
+        if (tax <= 3000) {
+          afterTotal = total - tax * 0.03;
+        } else if (tax > 3000 && tax <= 12000) {
+          afterTotal = total - 3000 * 0.03 - (tax - 3000) * 0.1;
+
+
+        } else if (tax > 12000 && tax <= 25000) {
+          afterTotal = total - 3000 * 0.03 - 9000 * 0.1 - (tax - 12000) * 0.2;
+        } else if (tax > 25000 && tax <= 35000) {
+          afterTotal = total - 3000 * 0.03 - 9000 * 0.1 - 13000 * 0.2 - (tax - 25000) * 0.25;
+        } else if (tax > 35000 && tax <= 55000) {
+          afterTotal = total - 3000 * 0.03 - 9000 * 0.1 - 13000 * 0.2 - 10000 * 0.25 - (tax - 35000) * 0.30;
+        } else if (tax > 55000 && tax <= 80000) {
+          afterTotal = total - 3000 * 0.03 - 9000 * 0.1 - 13000 * 0.2 - 10000 * 0.25 - 20000 * 0.30 - (tax - 55000) * 0.35;
+        } else if (tax > 80000) {
+          afterTotal = total - 3000 * 0.03 - 9000 * 0.1 - 13000 * 0.2 - 10000 * 0.25 - 20000 * 0.30 - 25000 * 0.35 - (tax - 80000) * 0.45;
+        }
+
+      } else {
+        afterTotal = total;
+
+      }
       this.setData({
-        infoMess:'',
-        userName:'用户名：'+this.data.userN,
-        passWd:'密码：'+this.data.passW
+        infoMess: afterTotal,
       })
+
+
     }
   },
   //重置按钮点击事件
-  resetBtnClick:function(e){
+  resetBtnClick: function(e) {
     this.setData({
       infoMess: '',
-      userName: '',
-      userN:'',
-      passWd: '',
-      passW:'',
+      calName: '',
+      calN: ''
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
 })
